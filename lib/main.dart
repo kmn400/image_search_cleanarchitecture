ca332c00e9a8e811f1be5f29a597fabbeca3d590
minho <kmn400@nate.com>
2022-02-01 15:24:49 +0900

    220201_clean_architecture3

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,15 @@
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
-import 'package:image_search_0130/data/data_source/pixabay_api.dart';
-import 'package:image_search_0130/domain/use_case/get_photos_use_case.dart';
+import 'package:image_search_0130/data/di/provider_setup.dart';
 import 'package:image_search_0130/presentation/home/home_screen.dart';
-import 'package:image_search_0130/presentation/home/home_view_model.dart';
 import 'package:provider/provider.dart';
 
-import 'data/repository/photo_api_repository_impl.dart';
-
 void main() {
-  runApp(const MyApp());
+  runApp(
+    MultiProvider(
+      providers: globalProviders,
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -23,11 +23,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: ChangeNotifierProvider(
-        create: (_) => HomeViewModel(GetPhotosUseCase(
-            PhotoApiRepositoryImpl(PixabayApi(http.Client())))),
-        child: const HomeScreen(),
-      ),
+      home: const HomeScreen(),
     );
   }
 }
